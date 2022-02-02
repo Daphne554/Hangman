0cfa6a1d8a5c1abc37e59126e5c50804d0e0536a
Daphne554 <daphnekodua@gmail.com>
2022-02-02 15:00:51 +0000

    main logic

--- hangman.py
+++ hangman.py
@@ -6,9 +6,38 @@ for line in DictFile:
 def hangman():
 
     word = (dictionary[random.randint(0, len(dictionary) - 1)]).lower()
+    validLetters = "abcdefghijklmnopqrstuvwxyz"
+    attempts = 10
+    guessmade = ''
+    while len(word)> 0:
+        main = ""
+        missed = 0
+        for letter in word:
+            if letter in guessmade:
+                main = main + letter
+            else:
+                main = main + "_" + ""
+            if main == word:
+                print(main)
+                print("You win")
+                break
+
+
+
+        if guess in validLetters:
+            guessmade = guessmade + guess
+        else:
+            print("Enter valid character")
+            guess = input()
+        if guess not in word:
+            attempts = attempts - 1
+
+
+
 
 name = input("Enter your name: ")
 print(f"Welcome {name}! ")
 print("You have 10 attempts to guess a word.")
 
 print()
+hangman()
