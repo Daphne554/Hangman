50f155963612b9b31ef070e0e58ce93da7ba290d
Daphne554 <daphnekodua@gmail.com>
2022-02-03 13:26:37 +0000

    hangman completed

--- hangman.py
+++ hangman.py
@@ -2,42 +2,103 @@ import random
 
 def hangman():
 
-    word = ( "Nigeria","Ethiopia","Democratic Republic of the Congo","Egypt","South Africa","Tanzania","Kenya",\
-             "Uganda","Algeria","Sudan","Morocco","Angola","Ghana","Cameron","Madagascar","Mozambique","IvoryCoast",\
-             "Niger","Mali","BurkinaFaso","Malawi","Chad","Somalia","Zimbabwe","Zambia","Senegal","South Sudan",\
-             "Rwanda","Guinea","Benin","Tunisia","Burundi","SierraLeone","Togo","Libya","Congo""Liberia",\
-             "CentralAfricanRepublic","Mauritania","Eritrea","Namibia","Gambia","Botswana","Gabon","Lesotho",\
-             "Guinea-Bissau","EquatorialGuinea","Mauritius","Eswatini","Djibouti","Comoros","CapeVerde","WesternSahara"\
-                 ,"Mayotte","Seychelles")
+    word = random.choice(["nigeria","ethiopia","congo","egypt","southafrica","tanzania","kenya",
+             "uganda","algeria","sudan","morocco","angola","ghana","cameron","madagascar","mozambique","ivorycoast",
+             "niger","mali","burkinafaso","malawi","chad","somalia","zimbabwe","zambia","senegal","southsudan",
+             "rwanda","guinea","benin","tunisia","burundi","sierraleone","togo","libya","congo","liberia",
+             "centralafricanrepublic","mauritania","eritrea","namibia","gambia","botswana","gabon","lesotho",
+             "guineabissau","equatorialguinea","mauritius","eswatini","djibouti","comoros","capeverde","westernsahara"
+                 ,"mayotte","seychelles"])
+
     validLetters = "abcdefghijklmnopqrstuvwxyz"
     attempts = 10
     guessmade = ''
 
-    while len(word)> 0:
+    while len(word) > 0:
         main = ""
         missed = 0
+
         for letter in word:
             if letter in guessmade:
                 main = main + letter
             else:
-                main = main + "_" + ""
-            if main == word:
-                print(main)
-                print("You win")
-                break
+                main = main + "_" + " "
+        if main == word:
+            print(main)
+            print("You win")
+            break
 
-            print("Guess the word: ", main)
-            guess = input()
+        print("Guess the word: " , main)
+        guess = input()
 
         if guess in validLetters:
             guessmade = guessmade + guess
         else:
             print("Enter valid character")
             guess = input()
+
         if guess not in word:
             attempts = attempts - 1
+            if attempts == 9:
+                print("9 turns left")
+                print("-------------")
+            if attempts == 8:
+                print("8 turns left")
+                print("-------------")
+                print("      0      ")
+            if attempts == 7:
+                print("7 turns left")
+                print("-------------")
+                print("      0      ")
+                print("      |      ")
+            if attempts == 6:
+                print("6 turns left")
+                print("-------------")
+                print("      0      ")
+                print("      |      ")
+                print("     /       ")
+            if attempts == 5:
+                print("5 turns left")
+                print("-------------")
+                print("      0      ")
+                print("      |      ")
+                print("     / \     ")
 
+            if attempts == 4:
+                print("4 turns left")
+                print("-------------")
+                print("    \ 0      ")
+                print("      |      ")
+                print("     / \     ")
 
+            if attempts == 3:
+                print("3 turns left")
+                print("-------------")
+                print("    \ 0 /    ")
+                print("      |      ")
+                print("     / \     ")
+
+            if attempts == 2:
+                print("2 turns left")
+                print("-------------")
+                print("    \ 0_ /   ")
+                print("      |      ")
+                print("     / \     ")
+
+            if attempts == 1:
+                print("1 turn left")
+                print("-------------")
+                print("    \ 0_|/   ")
+                print("      |      ")
+                print("     / \     ")
+
+            if attempts == 0:
+                print("You lose ")
+                print("You let a kind man die")
+                print("------------")
+                print("      0_|  ")
+                print("     /|\    ")
+                print("     / \     ")
 
 
 name = input("Enter your name: ")
